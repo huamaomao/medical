9a734096119622849c502cbbfb512165112a790e
Silent <467953673@qq.com>
2015-04-21 23:59:10 +0800

    2.1   布局改动

---
+++ doctor/src/main/java/com/rolle/doctor/viewmodel/UserModel.java
@@ -0,0 +1,26 @@
+package com.rolle.doctor.viewmodel;
+
+import com.android.common.viewmodel.ViewModel;
+import com.litesuits.http.request.content.StringBody;
+import com.rolle.doctor.util.RequestApi;
+
+/**
+ * Created by Administrator on 2015/4/21 0021.
+ */
+public class UserModel  extends ViewModel {
+    /****
+     * 获取个人资料
+     */
+    public void requestModel(String token){
+
+        //execute(RequestApi.requestUserInfo(token));
+    }
+
+    /*****
+     *
+     * @param
+     */
+    public void requestModel(){
+
+    }
+}
